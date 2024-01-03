3296dc531607185d1d6a035c44b9ab3306fe7d6a
aya <yokaosamaa640@gmail.com>
2024-01-03 21:06:19 +0200

    apply token in register view

--- lib/register_featcher/domain/cubit/register_cubit.dart
+++ lib/register_featcher/domain/cubit/register_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:e_commerce_app/Login_featcher/data/shared_pre.dart';
 
 import 'package:e_commerce_app/network/dio_helper.dart';
 import 'package:e_commerce_app/register_featcher/domain/cubit/register_states.dart';
@@ -13,6 +14,8 @@ class RegisterCubit extends Cubit<RegisterStates> {
     DioEcommerce.postRegisterData(name, phone, email, password).then((value) {
       // print(value);
       responseRegisterValue = value;
+      var token = responseRegisterValue!.data['data']['token'];
+      SharedPref.setToken(token);
       emit(PostRegisterSucessState());
     }).catchError((error) {
       // print('error from postLoginData is: $error');
